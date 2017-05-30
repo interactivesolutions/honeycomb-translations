7da1f1a1fe4de590c67ede892196c6c08cad48f8
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-05-30 09:50:28 +0300

    Added core translations

---
+++ src/resources/lang/en/core.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    "copyright" => "©",
+    "list" => "List",
+    "core" => "All",
+    "unknown_action" => "Unknown action:",
+    "nothing_to_delete" => "Nothing to delete",
+    "nothing_to_restore" => "Nothing to restore",
+    "language_not_found" => "Language not found",
+    "id" => "ID",
+    "name" => "Name",
+    "description" => "Description",
+    "label" => "Label",
+    "title" => "Title",
+    "index" => "Index",
+    "filter" => "Filter",
+    "administration" => "Administration",
+    "dev_env" => "Development environment",
+
+    "buttons" => [
+        "submit" => "Submit"
+    ],
+
+    "weekdays" => "Weekdays",
+    "weekday" => [
+        "monday" => "Monday",
+        "tuesday" => "Tuesday",
+        "wednesday" => "Wednesday",
+        "thursday" => "Thursday",
+        "friday" => "Friday",
+        "saturday" => "Saturday",
+        "sunday" => "Sunday"
+    ]
+];
